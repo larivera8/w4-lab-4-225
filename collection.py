5ac4c56c9a43dec6d416a6ed81aa2162a83143a3
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-30 23:47:55 -0500

    Update collection.py

--- collection.py
+++ collection.py
@@ -13,7 +13,12 @@ authors = {              # added o
     "Charles Dickens": "1870",
     "William Thackeray": "1863",
     "Anthony Trollope": "1882",
+    #No comma after "1889"
     "Gerard Manley Hopkins": "1889", }                       # added perentheses and comma
 
+'''
+for author, date in authors.items():
+    print("%s" % author + " died in " + "%s." % date)
+'''
 for author, date in authors.items():                           #added comma
     print("%s" % authors + " died in " + "%d." % Date)         #added perentheses
