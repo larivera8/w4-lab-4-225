682cf0ee9053a1fd6d3ad38d32b20d3748bd3479
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-30 23:49:48 -0500

    Update pirate.py

--- pirate.py
+++ pirate.py
@@ -1,6 +1,10 @@
 greeting = input("Hello, possible pirate! What's the password?")     # added quotation mark
 if greeting in ["Arrr!"]:                           # changed the close perentheses
 	print("Go away, pirate.")
+''' 
+else:
+    print("Greetings, hater of pirates!")
+'''
 elif greeting in ("Ahoy there landlovers"):         #added phase
 	print("Greetings, hater of pirates!")            # indented
 
