d09069852c3cec95cf95d1fe2580942803bcc92a
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-30 23:54:18 -0500

    Update time.py

--- time.py
+++ time.py
@@ -1,6 +1,17 @@
 currentTimeStr = input("What is the current time (in hours 0-24)?")       # change 23 hours to 24
 waitTimeStr = input("How many hours do you want to wait")                   # close parentheses added
 
+'''
+Name error
+current_time_str ==> currentTimeStr
+wait_time_str ==> waitTimeStr
+finalTime_Int ==> finalTimeInt
+
+Logic error: final time shows in hours 0-23
+finalTimeInt = (currentTimeInt + waitTimeInt) %24
+print(finalTimeInt)
+'''
+
 currentTimeInt = int(current_time_str)
 waitTimeInt = int(wait_time_str)
 
